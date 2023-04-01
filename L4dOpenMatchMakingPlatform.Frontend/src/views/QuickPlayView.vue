82c23042782eef5c9deaee60ed259875293e49e6
deximy <deximy@gmail.com>
2023-04-01 21:57:25 +0800

    feat: implement view of the quick play page

--- L4dOpenMatchMakingPlatform.Frontend/src/views/QuickPlayView.vue
+++ L4dOpenMatchMakingPlatform.Frontend/src/views/QuickPlayView.vue
@@ -4,21 +4,34 @@ import {ref} from "vue";
 
 import ModeSelection from "../components/ModeSelection.vue";
 
+import {NButton} from "naive-ui";
 
 const mode_controller = new ModeController();
+
 const mode_list = ref<GameMode[]>();
 mode_controller.GetModeList().then(
     result => {
         mode_list.value = result;
     }
 );
+
+const selected_mode_list = ref<GameMode[]>(new Array<GameMode>());
 </script>
 
 <template>
     <div class="quick-play-view">
+        <h1 class="title">选择匹配模式</h1>
         <ModeSelection
             :mode_list="mode_list"
         />
+        <div class="start-play">
+            <n-button
+                size="large"
+                :disabled="selected_mode_list.length === 0"
+            >
+                开始
+            </n-button>
+        </div>
     </div>
 </template>
 
@@ -26,5 +39,15 @@ mode_controller.GetModeList().then(
 .quick-play-view {
     width: 100%;
     height: 100%;
+}
+
+.title {
+    text-align: center;
+}
+
+.start-play {
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 </style>
