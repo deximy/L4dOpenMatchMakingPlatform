4d82d13faf3ea271ed80865643df50e90d6113eb
deximy <deximy@gmail.com>
2023-05-16 00:48:21 +0800

    feat: add player profile component to the frontend

---
+++ L4dOpenMatchMakingPlatform.Frontend/src/components/PlayerProfileTag.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import {NCard} from "naive-ui";
+import {NIcon} from "naive-ui";
+import {NTag} from "naive-ui";
+
+import {GetUserIdFromToken} from "../apis/Authentication";
+import {ref} from "vue";
+const user_id = ref<string | null>(null);
+GetUserIdFromToken().then(
+    (result) => {
+        user_id.value = result;
+    }
+)
+</script>
+
+<template>
+    <div class="player-profile">
+        <n-card
+            :bordered="false"
+            content-style="padding: 0; display: flex; align-items: center;"
+        >
+            <n-icon :size="24">
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32">
+                    <path d="M16 4a5 5 0 1 1-5 5a5 5 0 0 1 5-5m0-2a7 7 0 1 0 7 7a7 7 0 0 0-7-7z" fill="currentColor"></path>
+                    <path d="M26 30h-2v-5a5 5 0 0 0-5-5h-6a5 5 0 0 0-5 5v5H6v-5a7 7 0 0 1 7-7h6a7 7 0 0 1 7 7z" fill="currentColor"></path>
+                </svg>
+            </n-icon>
+            <span class="user-id" v-text="user_id"></span>
+            <n-tag class="user-role" size="small" type="info">管理员</n-tag>
+        </n-card>
+    </div>
+</template>
+
+<style scoped>
+.user-id {
+    margin-left: 1em;
+}
+
+.user-role {
+    margin-left: 5px;
+}
+</style>
