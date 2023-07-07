13e1523ee3f14be46206bd878f69a6c517626771
deximy <deximy@gmail.com>
2023-07-07 15:27:06 +0800

    feat: add model of players

---
+++ L4dOpenMatchMakingPlatform.Backend/Models/PlayerModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace L4dOpenMatchMakingPlatform.Backend.Models
+{
+    public class PlayerModel
+    {
+        [Key]
+        public Guid id { get; set; }
+
+        public PlayerStatus status { get; set; }
+
+
+        public TeamModel? team { get; set; }
+
+        public ICollection<ServerEndpointModel>? server_owned { get; set; }
+
+        public ICollection<CustomGameModel>? lobby_owned { get; set; }
+    }
+
+    public enum PlayerStatus
+    {
+        Idle,
+        InQueue,
+        InLobby,
+        InGame
+    }
+}
