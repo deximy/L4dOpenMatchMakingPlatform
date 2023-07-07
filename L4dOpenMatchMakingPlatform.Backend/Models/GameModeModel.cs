47bbefc8e68e56af559c038d612f70c85542b9b4
deximy <deximy@gmail.com>
2023-07-07 14:01:25 +0800

    feat: add model of game modes

---
+++ L4dOpenMatchMakingPlatform.Backend/Models/GameModeModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace L4dOpenMatchMakingPlatform.Backend.Models
+{
+    public class GameModeModel
+    {
+        [Key]
+        public Guid id { get; set; }
+
+        public string name { get; set; }
+
+
+        public ICollection<TeamConfigModel> team_config { get; set; }
+
+        public ICollection<ServerEndpointModel> server_supported { get; set; }
+    }
+}
---
+++ L4dOpenMatchMakingPlatform.Backend/Models/TeamConfigModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace L4dOpenMatchMakingPlatform.Backend.Models
+{
+    public class TeamConfigModel
+    {
+        [Key]
+        public Guid id { get; set; }
+
+        public string default_name { get; set; }
+
+        public int max_player_count { get; set; }
+    }
+}
